bfd33a0db1be55707677a7e2031f4fd49e1c11c5
Jason <4100344+jannson@users.noreply.github.com>
2025-01-06 11:12:01 +0800

    fix pve uppercase

--- applications/app-meta-pve/config.sh
+++ applications/app-meta-pve/config.sh
@@ -2,7 +2,7 @@
 [ -z "$ISTORE_CONF_DIR" ] && exit 1
 
 uci -q batch <<-EOF >/dev/null || exit 1
-    set pve.@pve[0].config_path="$ISTORE_CONF_DIR/pve"
+    set pve.@pve[0].config_path="$ISTORE_CONF_DIR/PVE"
     commit pve
 EOF
 
