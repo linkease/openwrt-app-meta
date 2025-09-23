580ebc09a2f35aa7b40f4689489b2d7f69f08bf6
Jason <4100344+jannson@users.noreply.github.com>
2025-09-23 23:36:00 +0800

    add config and entry for istoreenhance (#537)
    * add config and entry
    
    * add meta autoconfig
    
    * add useless cache dir
    
    * fix error
    
    * create config paths default
    
    * bump version
    
    ---------
    
    Co-authored-by: Jason <xiaobao@linkease.com>

---
+++ applications/app-meta-istoreenhance/entry.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+ISTOREENHANCE_BIN=/usr/sbin/iStoreEnhance
+
+status(){
+    local host="$1"
+    [ -n "$host" ] || host=127.0.0.1
+    . /usr/share/libubox/jshn.sh
+    json_init
+    json_add_string "app" "istoreenhance"
+    json_add_boolean "docker" "0"
+
+    if [ -x "$ISTOREENHANCE_BIN" ]; then
+        json_add_boolean "running" "1"
+        local port=$(uci get istoreenhance.@istoreenhance[0].adminport 2>/dev/null)
+        local portsec=${port:-5003}
+        json_add_string "web" ":${portsec}"
+        json_add_string "href" "http://$host:${portsec}/"
+        json_add_string "protocol" http
+        json_add_string "port" "${portsec}"
+        json_add_boolean "deployed" "1"
+    else
+        json_add_boolean "running" "0"
+        json_add_boolean "deployed" "0"
+    fi
+
+    json_dump
+    json_cleanup >/dev/null
+}
+
+start(){
+    echo "Start not supported for iStoreEnhance" >&2
+    /etc/init.d/istoreenhance start
+    return 0
+}
+
+stop(){
+    echo "Stop not supported for iStoreEnhance" >&2
+    /etc/init.d/istoreenhance stop
+    return 0
+}
+
+ACTION=${1}
+shift 1
+[ -z "${ACTION}" ] && ACTION=help
+
+case ${ACTION} in
+  "status" | "start" | "stop")
+    ${ACTION} "$@"
+  ;;
+  *)
+    echo "Unknown Action" >&2
+    exit 1
+  ;;
+esac
