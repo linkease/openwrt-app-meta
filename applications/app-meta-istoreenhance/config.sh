a10385801a1bc4a9779cebde2c3fd9d422d94cef
Jason <xiaobao@linkease.com>
2025-09-23 18:49:15 +0800

    add config and entry

---
+++ applications/app-meta-istoreenhance/config.sh
@@ -0,0 +1,22 @@
+
+[ -z "$ISTORE_CONF_DIR" ] && exit 1
+[ -z "$ISTORE_CACHE_DIR" ] && exit 1
+
+ENABLED=1
+ISTORE_ACTION=start
+if [ -n "$ISTORE_DONT_START" ]; then
+    ENABLED=0
+    ISTORE_ACTION=stop
+fi
+
+uci -q batch <<-EOF >/dev/null || exit 1
+    set istoreenhance.@istoreenhance[0].enabled=$ENABLED
+    set istoreenhance.@istoreenhance[0].cache='$ISTORE_CONF_DIR/iStoreEnhance'
+    commit istoreenhance
+EOF
+
+if [ -f /etc/init.d/istoreenhance ]; then
+    /etc/init.d/istoreenhance $ISTORE_ACTION || [ stop = $ISTORE_ACTION ]
+else
+    /etc/init.d/istoreenhance $ISTORE_ACTION || [ stop = $ISTORE_ACTION ]
+fi
