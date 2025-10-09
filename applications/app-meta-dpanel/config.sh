e7b250e652dd4a85b3ea445ec3decdf57a3537bf
Jason <xiaobao@linkease.com>
2025-10-09 20:36:23 +0800

    auto install and config dpanel

--- applications/app-meta-dpanel/config.sh
+++ applications/app-meta-dpanel/config.sh
@@ -3,11 +3,11 @@
 [ -z "$ISTORE_CONF_DIR" ] && exit 1
 
 uci -q batch <<-EOF >/dev/null || exit 1
-	set dpanel.@main[0].config_path="$ISTORE_CONF_DIR/dpanel"
+	set dpanel.@main[0].config_path="$ISTORE_CONF_DIR/DPanel"
 	commit dpanel
 EOF
 
 ISTORE_ACTION=install
 [ -z "$ISTORE_DONT_START" ] || ISTORE_ACTION=stop
 
-/usr/libexec/dpanel/dpanel.sh $ISTORE_ACTION || [ stop = $ISTORE_ACTION ]
+/usr/libexec/istorec/dpanel.sh $ISTORE_ACTION || [ stop = $ISTORE_ACTION ]
