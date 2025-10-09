e8d3b4d040121257c32c21c22df0ba9402150882
Jason <4100344+jannson@users.noreply.github.com>
2025-10-09 19:57:56 +0800

    add dpanel (#538)
    * add dpanel
    
    * add logo for dpanel
    
    * add logo
    
    ---------
    
    Co-authored-by: Jason <xiaobao@linkease.com>

---
+++ applications/app-meta-dpanel/config.sh
@@ -0,0 +1,13 @@
+
+# Require a config directory to be provided by the installer environment
+[ -z "$ISTORE_CONF_DIR" ] && exit 1
+
+uci -q batch <<-EOF >/dev/null || exit 1
+	set dpanel.@main[0].config_path="$ISTORE_CONF_DIR/dpanel"
+	commit dpanel
+EOF
+
+ISTORE_ACTION=install
+[ -z "$ISTORE_DONT_START" ] || ISTORE_ACTION=stop
+
+/usr/libexec/dpanel/dpanel.sh $ISTORE_ACTION || [ stop = $ISTORE_ACTION ]
