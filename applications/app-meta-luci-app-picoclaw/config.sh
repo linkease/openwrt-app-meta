4a29b0bf6308b83c9dcdd8895e1f040563261c95
KUAN YAN <yankuan@zuaa.zju.edu.cn>
2026-03-29 16:27:51 +0800

    Simplify config.sh for picoclaw

--- applications/app-meta-luci-app-picoclaw/config.sh
+++ applications/app-meta-luci-app-picoclaw/config.sh
@@ -1,10 +1,1 @@
-#!/bin/sh
-# Auto-configuration for luci-app-picoclaw
-case "$1" in
-    path)
-        echo "Configuring PicoClaw data path..."
-        ;;
-    enable)
-        /etc/init.d/picoclaw enable 2>/dev/null
-        ;;
-esac
+/usr/libexec/istorec/picoclaw.sh install
