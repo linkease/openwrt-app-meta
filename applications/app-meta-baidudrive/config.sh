5ce695266210f556f743062e9f4bc77376977efd
Liangbin Lian <1129525450@qq.com>
2026-06-02 15:02:54 +0800

    Fix procd config sh (#639)
    * tree-wide: fix config.sh for all procd enabled init
    
    * baidudrive: update deps and desc

--- applications/app-meta-baidudrive/config.sh
+++ applications/app-meta-baidudrive/config.sh
@@ -2,10 +2,8 @@
 [ -z "$ISTORE_CONF_DIR" ] && exit 1
 
 ENABLED=1
-ISTORE_ACTION=start
 if [ -n "$ISTORE_DONT_START" ]; then
 	ENABLED=0
-	ISTORE_ACTION=stop
 fi
 
 mkdir -p "$ISTORE_CONF_DIR/BaiduDrive" || exit 1
@@ -16,4 +14,4 @@ uci -q batch <<-EOF >/dev/null || exit 1
 	commit baidudrive
 EOF
 
-/etc/init.d/baidudrive $ISTORE_ACTION || [ stop = $ISTORE_ACTION ]
+/etc/init.d/baidudrive restart || true
