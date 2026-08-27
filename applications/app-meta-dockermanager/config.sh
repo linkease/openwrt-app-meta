ecb89eb3d3adc5c50fbe5ae7552242bab43a5dbc
Jason <4100344+jannson@users.noreply.github.com>
2026-08-27 02:11:54 +0000

    add app-meta-dockermanager (#675)
    * add app-meta-dockermanager
    
    * dockermanager fix
    
    ---------
    
    Co-authored-by: jannson <jannon.wei@gmail.com>

---
+++ applications/app-meta-dockermanager/config.sh
@@ -0,0 +1,20 @@
+[ -z "$ISTORE_CONF_DIR" ] && exit 1
+
+ENABLED=1
+if [ -n "$ISTORE_DONT_START" ]; then
+	ENABLED=0
+fi
+
+mkdir -p "$ISTORE_CONF_DIR/DockerManager" || exit 1
+
+uci -q batch <<-EOF >/dev/null || exit 1
+	set dockermanager.@dockermanager[0].enabled=$ENABLED
+	set dockermanager.@dockermanager[0].data_dir="$ISTORE_CONF_DIR/DockerManager"
+	set dockermanager.@dockermanager[0].port="8192"
+	set dockermanager.@dockermanager[0].bind_addr="0.0.0.0"
+	set dockermanager.@dockermanager[0].base_path="/apps/dockermanager/"
+	commit dockermanager
+EOF
+
+/etc/init.d/dockermanager restart || true
+[ -x /etc/init.d/linkease ] && /etc/init.d/linkease restart >/dev/null 2>&1 &
