240a95d0d8d19135f0cb975c4cc5cfe7ad54d7f3
jannson <jannon.wei@gmail.com>
2026-09-01 10:10:28 +0000

    upgrade linkeasefull 3.0.16

--- applications/app-meta-dockermanager/entry.sh
+++ applications/app-meta-dockermanager/entry.sh
@@ -11,26 +11,33 @@ status(){
 	local port
 	port="$(uci get dockermanager.@dockermanager[0].port 2>/dev/null)"
 	local portsec=${port:-8192}
-	local base_path
-	base_path="$(uci get dockermanager.@dockermanager[0].base_path 2>/dev/null)"
-	local basepath=${base_path:-/apps/dockermanager/}
-	case "$basepath" in
-		/*) ;;
-		*) basepath="/$basepath" ;;
+		local base_path
+		base_path="$(uci get dockermanager.@dockermanager[0].base_path 2>/dev/null)"
+		local basepath=${base_path:-/apps/dockermanager/}
+		local external_port_enabled
+		external_port_enabled="$(uci get dockermanager.@dockermanager[0].external_port_enabled 2>/dev/null)"
+		case "$basepath" in
+			/*) ;;
+			*) basepath="/$basepath" ;;
 	esac
 	case "$basepath" in
 		*/) ;;
 		*) basepath="$basepath/" ;;
 	esac
 
-	if pidof docker-manager >/dev/null 2>&1; then
-		json_add_boolean "running" "1"
-		json_add_string "web" ":${portsec}"
-		json_add_string "href" "http://$host:${portsec}${basepath}"
-		json_add_string "protocol" http
-		json_add_string "port" "${portsec}"
-		json_add_boolean "deployed" "1"
-	else
+		if pidof docker-manager >/dev/null 2>&1; then
+			json_add_boolean "running" "1"
+			if [ "$external_port_enabled" = "1" ]; then
+				json_add_string "web" ":${portsec}"
+				json_add_string "href" "http://$host:${portsec}${basepath}"
+				json_add_string "port" "${portsec}"
+			else
+				json_add_string "web" "${basepath}"
+				json_add_string "href" "/cgi-bin/luci/admin/services/dockermanager/open"
+			fi
+			json_add_string "protocol" http
+			json_add_boolean "deployed" "1"
+		else
 		json_add_boolean "running" "0"
 		if [ -x /etc/init.d/dockermanager ]; then
 			json_add_boolean "deployed" "1"
