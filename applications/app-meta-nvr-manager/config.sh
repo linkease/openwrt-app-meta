573719a0c44d8db6c7db28c307a3ca8286d98dd9
Gilbert Liu <liuyuhao1117@outlook.com>
2026-09-26 22:23:12 +0800

    fix(config.sh): do not force enable/start if data_dir is not yet configured

--- applications/app-meta-nvr-manager/config.sh
+++ applications/app-meta-nvr-manager/config.sh
@@ -2,10 +2,12 @@
 # Auto-configuration hook for nvr-manager
 set -e
 
+data_dir=$(uci -q get nvr-manager.config.data_dir || true)
+
 if [ -n "$ISTORE_DONT_START" ] && [ "$ISTORE_DONT_START" = "1" ]; then
 	uci set nvr-manager.config.enabled='0'
 	uci commit nvr-manager
-else
+elif [ -n "$data_dir" ]; then
 	uci set nvr-manager.config.enabled='1'
 	uci commit nvr-manager
 	/etc/init.d/nvr-manager restart 2>/dev/null || true
